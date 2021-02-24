ad7ebffcb505c829e392bf98b97b385c60b33877
Oscar Nord <oscar.nord@gmail.com>
2021-02-24 22:36:01 +0100

    Cleanup and simplify example

--- app/detector.py
+++ app/detector.py
@@ -3,48 +3,24 @@ import pytesseract
 
 
 print('OpenCV version: ' + cv2.__version__)
-# list with keywords
+# hardcoded list with keywords for now
 DICT = ['director', 'directed', 'written', 'writer',
         'producer', 'produced', 'production', 'lead']
-# Get path to file
-path = input('Enter path to video file: ')
-# Get frame step
-step = int(input('frame step: '))
-if step <= 0:
-    step = 1
-print(step)
 
-success = True
-frameStep = 0
-counter = 0
+path = input('Enter path to file or hls manifest: ')
 
 vidcap = cv2.VideoCapture(path)
-frame = vidcap.read()
+success,frame = vidcap.read()
+framenbr = 0
 
 print('Working...')
 while success:
-    if frameStep == step and frame is not None:
-        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray_image = cv2.resize(gray_image, None, fx=2,
-                                fy=2, interpolation=cv2.INTER_LINEAR)
-        blur_image = cv2.GaussianBlur(gray_image, (11, 11), 0)
-        thresh = cv2.threshold(
-            blur_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-
-        opening = cv2.morphologyEx(
-            thresh, cv2.MORPH_OPEN, kernel, iterations=1)
-        invert = 255 - opening
-
-        data = pytesseract.image_to_string(
-            invert, lang='eng', config='--psm 6').lower()
-
-        if any(word in data for word in DICT):
-            print(data + '\nFrame number: ' + str(counter))
-
-        frameStep = 0
-    success, frame = vidcap.read()
-    frameStep += 1
-    counter += 1
-
-print('Number of frames: ' + str(counter))
+    success,frame = vidcap.read()
+    if not success:
+        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+    data = pytesseract.image_to_string(gray, lang='eng', config='--psm 6').lower()
+    if any(word in data for word in DICT):
+        print(data + '\nFrame number: ' + str(framenbr))
+    framenbr += 1
